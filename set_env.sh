a197ecce0a5ce48c3835dd15b360b17f1ddbc623
Djibril.6et <djibril.6et@gmail.com>
2023-10-27 15:57:25 +0000

    commit

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USER=udagramDb
+export POSTGRES_PASSWORD=root
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=udagramDb
+export AWS_BUCKET=udagram-bucket-1234567 # Please dont change this
+export AWS_REGION=us-east-1 #Please dont change this
+export AWS_PROFILE=default #Please dont change this
+export JWT_SECRET=testing #Please dont change this
+export URL=http://localhost:8100 #Please dont change this
